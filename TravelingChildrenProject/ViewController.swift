76024f4215c96de72de806dbd40ef766d6077800
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-11-10 14:13:43 -0500

    Initial Commit

---
+++ TravelingChildrenProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravelingChildrenProject
+//
+//  Created by Alexander Rhett Crammer on 11/10/15.
+//  Copyright © 2015 CDTDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
